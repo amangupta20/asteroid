8f3a2bd804024b04c8f6533dbb558ac59a397817
Aman <amanphankhu@gmail.com>
2026-02-05 21:28:23 +0530

    shooting limiting

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED,PLAYER_SPEED,PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED,PLAYER_SPEED,PLAYER_SHOOT_SPEED,PLAYER_SHOOT_COOLDOWN_SECONDS
 import pygame
 from shot import Shot
 class Player(CircleShape):
     def __init__(self, x: int, y: int):
         super().__init__(x, y,PLAYER_RADIUS)
         self.rotation = 0
+        self.cd=0
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -19,7 +20,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.cd-=dt
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -36,6 +37,9 @@ class Player(CircleShape):
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
     def shoot(self):
+        if self.cd>0:
+            return
+        self.cd=PLAYER_SHOOT_COOLDOWN_SECONDS
         obj=Shot(self)
         obj.velocity=pygame.Vector2(0, 1).rotate(self.rotation)*PLAYER_SHOOT_SPEED
 
