2bc7dcd36a06dd156c3a9f8784607802e4544eb0
Aman <amanphankhu@gmail.com>
2026-02-05 21:35:29 +0530

    shot interaction

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH,ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -8,3 +10,9 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen,"white",self.position,self.radius ,LINE_WIDTH)
     def update(self,dt):
         self.position += dt * self.velocity
+    def split(self):
+        self.kill()
+        if self.radius <=ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random.uniform(0,1)
